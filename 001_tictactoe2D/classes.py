50454f5370c6cdea209efb22ac52a17f2c33d3c8
stepanhampl <koblihh1@gmail.com>
2022-08-17 17:38:13 +0200

    add_move()
    app can validate given input (fits into range?)
    can do everything except validating, if there is a winner
    method status() is still empty

--- 001_tictactoe2D/classes.py
+++ 001_tictactoe2D/classes.py
@@ -1,6 +1,3 @@
-# class Box:
-#     pass
-
 class Field:  # grid
 
     def __init__(self, dimensions: tuple[int, int]):  # accepts dimensions
@@ -9,10 +6,13 @@ class Field:  # grid
         self.height = dimensions[1]
 
         if self.width > 30 and self.width < 3 or self.height > 99 and self.height < 3:
-            raise Exception("Sorry, width must be between 3 and 30, height between 3 and 99")  # width: because it wouldnt fit in window...
+            # width: because it wouldnt fit in window...
+            raise Exception(
+                "Sorry, width must be between 3 and 30, height between 3 and 99")
         self.rows = []
         for _ in range(self.height):
-            self.rows.append([' '] * self.width)    # set up 'empty' boxes while init
+            # set up 'empty' boxes while init
+            self.rows.append([' '] * self.width)
         # now    # self.rows = [ [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '] ]
         # later  # self.rows = [ ['X', ' ', ' ', ' '], ['O', 'X', ' ', ' '], [' ', 'X', ' ', 'O'], ['X', ' ', ' ', ' '], [' ', 'O', 'O', ' '] ]
 
@@ -26,38 +26,48 @@ class Field:  # grid
     def render(self):
         """creates multiline string, which is current gamefield when printed"""
         rows = list(self.rows)
-        header_sequence = list(range(1, self.width + 1))          # [1, 2, 3, 4, 5]
-        header_sequence = [str(num) if len(str(num)) > 1 else ' ' + str(num) for num in header_sequence]   # ['1', '2', '3', '4', '5']
-        rendered_field = 'Y' + u'\u2193' + 'X' + u'\u2192' + ') '.join(header_sequence) + ')\n'    # 'y, arrow, x, arrow  1) 2) 3) 4) 5)' ...example = row number 0
+        header_sequence = list(range(1, self.width + 1)
+                               )          # [1, 2, 3, 4, 5]
+        header_sequence = [str(num) if len(str(num)) > 1 else ' ' + str(num)
+                           for num in header_sequence]   # ['1', '2', '3', '4', '5']
+        # 'y, arrow, x, arrow  1) 2) 3) 4) 5)' ...example = row number 0
+        rendered_field = 'Y' + u'\u2193' + 'X' + \
+            u'\u2192' + ') '.join(header_sequence) + ')\n'
         for i, row in enumerate(rows):         # add each row
             index = i + 1  # so it doesnt start with 0
-            first_col_num = ' ' + str(index) if index < 10 else str(index)      # e.g. ' 5)' or '50)' ... second time no space
-            rendered_field += first_col_num + ') ' + ' T '.join(row) + '\n'  # add box in left column... '1) ' AND add row itself
+            # e.g. ' 5)' or '50)' ... second time no space
+            first_col_num = ' ' + str(index) if index < 10 else str(index)
+            # add box in left column... '1) ' AND add row itself
+            rendered_field += first_col_num + ') ' + ' T '.join(row) + '\n'
         return rendered_field
 
+    def validate_move(self, x, y):
+        """returns True if move can be inserted, otherwise returns False"""
 
-    # def render(self):
-    #     boxes = list(self.rows)
-    #     rendered_field = ''
-    #     for index, box in enumerate(boxes):
-    #         if index % self.width == 0:
-    #             rendered_field += box + '\n'
-    #         else:    
-    #             rendered_field += box + '|'
-    #     return rendered_field  # returns string with newline chars - it will be printed
+        # is in correct range?
+        if y < 1 or len(self.rows) < y:
+            return 'y is out of range'
+        if x < 1 or len(self.rows[y - 1]) < x:
+            return 'x is out of range'
 
-class Sign(Field):  # both X and O
-    def __init__(self, location: tuple[int, int], which: str):
-        self.x_axis = location[0]
-        self.y_axis = location[1]
-        self.which = which
+        if not self.rows[y - 1][x - 1] == ' ':
+            return 'desired box is full'
+        else:
+            return False
+
+    def add_move(self, location: tuple[int, int], which: str):      # False if loop should stop
+        """inserts move into 'rows' list, but if move is invalid returns string with reason 
+        (it means if box is full or out of range), otherwise False"""
+        x = location[0]     # x-axis
+        y = location[1]     # y-axis
+        validate = self.validate_move(x, y)     # False if box is empty and exists
+        if not validate:    # box is empty and exists
+            self.rows[y - 1][x - 1 : x] = which
+        return validate
 
 
-class Ex(Sign):  # X
-    def __init__(self, location: tuple[int, int]):
-        super().__init__(location, 'X')
-
-
-class Oh(Sign):  # O
-    def __init__(self, location: tuple[int, int]):
-        super().__init__(location, 'O')
+# class Sign(Field):  # both X and O
+#     def __init__(self, location: tuple[int, int], which: str):
+#         x_axis = location[0]
+#         y_axis = location[1]
+#         super().insert_in_rows(x_axis, y_axis)
--- 001_tictactoe2D/tictactoe.py
+++ 001_tictactoe2D/tictactoe.py
@@ -8,12 +8,25 @@ def run_game():
     width = int(input("Insert desired width of gamefield: "))
     height = int(input("Insert desired height of gamefield: "))
     playfield = Field((width, height))  # dimensions # init of playfield
-    
-    # while (status := playfield.status()):  # while game doesn't end
-    print(playfield.render())  # newly rendered playfield is printed
-        # for char in ['X', 'Y']:
-        #     print(f"{char}'s turn:")
-        #     x_axis = input('Where on x-axis?')
-        #     y_axis = input('Where on y-axis?')
+
+    while True:  # while game doesn't end
+        for player in ['X', 'O']:
+            print(playfield.render())  # newly rendered playfield is printed
+            while True:
+                print(f"{player}'s turn:")
+                x_axis = int(input('Where on x-axis?'))
+                y_axis = int(input('Where on y-axis?'))
+                # False if box is full, otherwise True # saves player's move - it will be shown by the next playfield.render()
+                if message := playfield.add_move((x_axis, y_axis), player):
+                    print(f'Sorry, {message}.')
+                else:
+                    break
+            if status := playfield.status() != True:  # if game ends # if status explicitly doesn't equal True
+                if status == False:
+                    print("It's a draw")
+                else:
+                    print(f"Congratualtions, {status} won")
+                break
+
 
 run_game()
