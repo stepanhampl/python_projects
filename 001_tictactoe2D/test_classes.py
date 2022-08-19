b96cff500ed603a91fefae84703f34c88b3b32ba
stepanhampl <koblihh1@gmail.com>
2022-08-19 11:30:38 +0200

    more  tests
    horizontal, another players, bigger goal and bigger field

--- 001_tictactoe2D/test_classes.py
+++ 001_tictactoe2D/test_classes.py
@@ -1,13 +1,42 @@
-# import pytest
 import classes
 
-def test_status():
-    width = 3
-    height = 3
-    how_many_to_win = 3     # how many do you need to have in a row to win
-    players = ('X', 'O') 
-    playfield = classes.Field((width, height), how_many_to_win, players)
-
-    playfield.rows = [['X', 'X', 'X'], ['O', 'O', ' '], ['O', ' ', ' ']]
+# used inside test_status()
+def assist_status(dimensions: tuple, winner, players, goal, rows):
+    playfield = classes.Field(dimensions, goal, players)
+    playfield.rows = rows
     status = playfield.status()
-    assert status == 'X'
+    assert status == winner
+
+
+def test_status():
+    winner = 'X'
+    players = ('X', 'O')
+    goal = 3
+    dimensions = (3, 3)
+
+    # test horizontal evaluation
+    rows = [['X', 'X', 'X'], ['O', 'O', ' '], ['O', ' ', ' ']]
+    assist_status(dimensions, winner, players, goal, rows)
+    rows = [['X', 'O', 'O'], ['O', 'O', ' '], ['X', 'X', 'X']]
+    assist_status(dimensions, winner, players, goal, rows)
+    rows = [['X', 'O', 'X'], ['O', 'O', 'O'], ['X', ' ', ' ']]
+    winner = 'O'
+    assist_status(dimensions, winner, players, goal, rows)
+
+    # test another players
+    rows = [['Ň', 'Ř', 'Ň'], ['Ř', 'Ř', 'Ř'], ['Ň', ' ', ' ']]
+    players = ('Ř', 'Ň')
+    assist_status(dimensions, 'Ř', players, goal, rows)
+
+    # test bigger goal, bigger field
+    rows = [
+        [' ', ' ', ' ', ' ', ' ', ' '], 
+        ['O', 'X', 'O', 'O', 'X', 'O'],
+        [' ', ' ', ' ', ' ', ' ', ' '], 
+        ['O', 'O', 'X', 'O', 'O', ' '], 
+        [' ', ' ', ' ', ' ', ' ', ' '],
+        ['O', 'X', 'X', 'X', 'X', 'X'], ]
+    dimensions = (6, 6)
+    goal = 5
+    winner = 'X'
+    players = ('X', 'O')
